e29704d388812191bfcdf9d9ab84a41d910bedce
manuvidalamor <manuvidalamor@gmail.com>
2020-09-03 08:33:27 +0200

    Cambios API execución de Scrapers

--- tfgAPI/app/Console/Kernel.php
+++ tfgAPI/app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-      //$schedule->command('scrapers:all')->weekdays()->dailyAt('8:00');
-      //$schedule->command('scrapers:all')->weekends()->twiceDaily(13, 23);
+      $schedule->command('scrapers:all')->weekdays()->dailyAt('8:00');
+      $schedule->command('scrapers:all')->weekends()->twiceDaily(13, 23);
     }
 }
